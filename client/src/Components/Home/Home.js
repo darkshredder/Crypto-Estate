aa3d4c8da3de607cb742ea67880dd5bb2ae27708
smrpn <samarpan_d@pp.iitr.ac.in>
2020-10-03 16:26:28 +0530

    added home page

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
-import styles from "./Home.module.css";
+import React, { Component } from 'react'
+
+import NavBar from '../Utils/NavBar'
+import Stats from '../Utils//Stats'
+import OverView from '../Utils//OverView'
+import styles from './Home.module.css'
 export default class Home extends Component {
   render() {
     return (
       <div>
-        Yash
+        <NavBar />
+        <div className={styles.container}>
+          <div className={styles.chart}><Stats />
+          </div>
+          <div className={styles.overview}>
+            <OverView />
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
