ac5252c5565650618ecc7df4fae8f9870cd11f51
Darkshredder <srivastavayash58@gmail.com>
2020-10-03 12:28:42 +0530

    added home page

---
+++ client/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import styles from "./Home.module.css";
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        Yash
+      </div>
+    );
+  }
+}
